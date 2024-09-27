634e2e206944dd4e1876e4949637f347bb659d0c
smas93 <masellagiacinto@gmail.com>
2024-09-27 11:15:01 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "@mantine/core/styles.css";
 import { MantineProvider, Center, Box } from "@mantine/core";
-import { theme } from "./theme";
 import { AutocompleteLoading } from "./AutocompleteLoading";
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider>
       <Center mt={100}>
         <Box>
           <AutocompleteLoading />
